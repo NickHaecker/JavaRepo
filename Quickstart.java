f7dde57645489f1565d59ceeb11526acab78a071
NickHaecker <nick.philipp.haecker@hs-furtwangen.de>
2020-11-25 22:50:18 +0100

    Übung 1

--- Quickstart.java
+++ Quickstart.java
@@ -1,5 +1,33 @@
 class QuickStart {
+    private static int[] intArr = { 5, 24, 18, 76 };
+
+    public void tauschValue(int index) {
+        for (int i = 0; i <= intArr.length - 1; i++) {
+            if (i == index) {
+                int k = intArr[i];
+                int g = -1;
+                if (i - 1 >= 0) {
+                    g = intArr[i - 1];
+                }
+                if (g != -1) {
+                    intArr[i] = g;
+                    intArr[i - 1] = k;
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World.");
+        QuickStart wt = new QuickStart();
+
+        System.out.println("vorher:");
+        for (int i = 0; i < intArr.length; i++) {
+            System.out.println(intArr[i]);
+        }
+        wt.tauschValue(-1);
+        System.out.println("\nnachher:");
+        for (int i = 0; i < intArr.length; i++) {
+            System.out.println(intArr[i]);
+        }
     }
 }
